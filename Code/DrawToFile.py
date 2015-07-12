9da954c3278ca5f6ca26c6d41036117be5b6437b
Piripant <piripant@gmail.com>
2015-07-12 19:41:01 +0200

    Now centers the graph

--- Code/DrawToFile.py
+++ Code/DrawToFile.py
@@ -3,7 +3,7 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import math
 
-def saveinfile(outputfile="output.png", points=[], maxx=1, maxy=1, xsize=2048, ysize=2048):
+def saveinfile(outputfile="output.png", points=[], maxx=1, minx=1, maxy=1, miny=1, xsize=4096, ysize=4096):
     app = QCoreApplication([])
     size = QSize(xsize, ysize)
     img = QImage(size, QImage.Format_RGB32)
@@ -12,23 +12,23 @@ def saveinfile(outputfile="output.png", points=[], maxx=1, maxy=1, xsize=2048, y
     painter = QPainter(img)
     pen = QPen(Qt.black, 2, Qt.SolidLine)
     painter.setPen(pen)
-
-    S = min(xsize/maxx, ysize/maxy)
-    Sx = maxx*S
-    Sy = maxy*S
-    #Offx = Sx/2-xsize/2
-    #Offy = Sx/2-ysize/2
+    
+    gsizex = maxx-minx
+    gsizey = maxy-miny
+    
+    S = min(xsize/gsizex, ysize/gsizey)
+    Sx = gsizex*S
+    Sy = gsizey*S
     Offx = (xsize-Sx)/2
     Offy = (ysize-Sy)/2
 
 
     for i in range(0, len(points)):
         if i+1 < len(points):
-            startx = points[i][0]*S+Offx
-            starty = points[i][1]*S+Offy
-            endx = points[i+1][0]*S+Offx
-            endy = points[i+1][1]*S+Offy
-            print("x: " + str(startx) + ", y: " + str(endx))
+            startx = (points[i][0]-minx)*S+Offx
+            starty = (points[i][1]-miny)*S+Offy
+            endx = (points[i+1][0]-minx)*S+Offx
+            endy = (points[i+1][1]-miny)*S+Offy
             painter.drawLine(startx, starty, endx, endy)
 
     painter.end()
