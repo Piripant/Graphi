9f1ef5231c1cbd3577483628d1b5faa2067303be
Piripant <piripant@gmail.com>
2015-07-19 18:22:50 +0200

    Resolves correctly functions

--- Code/Equator.py
+++ Code/Equator.py
@@ -44,7 +44,8 @@ def operation(operator="", number=0, result=0):
 
 def deparenter(formula=[]):
     stack = [[]]
-    multchain = False
+    multchain = [False]
+    functchain = [False]
     for i in range(0, len(formula)):
         if i+1 < len(formula):
             nextchar = formula[i+1]
@@ -53,27 +54,64 @@ def deparenter(formula=[]):
         
         # Appends if (*/
         if formula[i] is "(":
+            multchain.append(False)
+            functchain.append(False)
             stack.append([])
         
-        elif nextchar in multopers and multchain is False:
-            multchain = True
+        elif nextchar in multopers and multchain[len(multchain)-1] is False:
+            multchain[len(multchain)-1] = True
             stack.append([])
-        
+
+        elif formula[i] in functions:
+            functchain[len(functchain)-1] = True
+            if nextchar in functions:
+                functchain.append(False)
+            stack.append([])
+
         # Appends char
         if formula[i] != "(" and formula[i] != ")":
             stack[len(stack)-1].append(formula[i])
-        
-        # Resolves if ) or not */ (a multiplication chain has stopped)
-        if formula[i] is ")":
-            oper = list(stack[len(stack)-1])
-            stack.pop()
-            stack[len(stack)-1].append(resolve(oper))
-        
-        elif formula[i] not in multopers and nextchar not in multopers and formula[i] is not "(" and nextchar is not "(" and multchain is True:
-            multchain = False
-            oper = list(stack[len(stack)-1])
-            stack.pop()
-            stack[len(stack)-1].append(resolve(oper))
+            if formula[i] in functions:
+                continue
+
+        rewhile = True
+        rechecked = False
+        while rewhile is True:
+            rewhile = False
+
+            # Resolves multiplication chain
+            if formula[i] not in multopers and nextchar not in multopers and formula[i] is not "(" and nextchar is not "(" and multchain[len(multchain)-1] is True:
+                multchain[len(multchain)-1] = False
+                oper = list(stack[len(stack)-1])
+                stack.pop()
+                stack[len(stack)-1].append(resolve(oper))
+
+            # Resolves functions
+            if functchain[len(functchain)-1] is True and nextchar not in functions and nextchar is not "(":
+                if len(functchain) > 1:
+                    functchain.pop()
+                else:
+                    functchain[len(functchain)-1] = False
+
+                oper = list(stack[len(stack)-1])
+                stack.pop()
+                stack[len(stack)-1].append(resolve(oper))
+                rewhile = True
+                continue
+
+            if rechecked is True:
+                break
+
+            # Resolves if )
+            if formula[i] is ")":
+                oper = list(stack[len(stack)-1])
+                stack.pop()
+                multchain.pop()
+                functchain.pop()
+                stack[len(stack)-1].append(resolve(oper))
+                rewhile = True
+
+            rechecked = True
 
     return stack[0]
 
@@ -83,27 +121,17 @@ def resolve(formula=[]):
     i = 0
     while i < len(formula):
         if type(formula[i]) is float:
-                result = operation("", formula[i], result)
-                i += 1
+            result = operation("", formula[i], result)
+            i += 1
 
         if i+1 < len(formula):
             if formula[i] in functions and type(formula[i+1]) is float:
                 result = operation(formula[i], functioner(formula[i], formula[i+1]), result)
-                i += 3
 
             elif formula[i] not in functions and type(formula[i+1]) is float:
                 result = operation(formula[i], formula[i+1], result)
-                i += 2
 
-        if i+2 < len(formula):            
-            if formula[i] not in functions and formula[i+1] in functions and type(formula[i+2]) is float:
-                result = operation(formula[i], functioner(formula[i], functioner(formula[i+1], formula[i+2])), result)
-                i += 3
-				
-        if i+3 < len(formula):
-            if formula[i] not in functions and formula[i+1] in functions and formula[i+2] in functions and type(formula[i+3]) is float:
-                result = operation(formula[i], functioner(formula[i+1], functioner(formula[i+2], formula[i+3])), result)
-                i += 4
+            i += 2
 
     return result
 
